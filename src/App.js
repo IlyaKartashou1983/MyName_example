57c0a34de080e92bf42e7699feacbd2aa5b7a729
KartachovIA <ikartachov@yandex.ru>
2020-02-10 00:11:01 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MyName from "./MyName";
+
+function App() {
+  return (
+    <div className="App">
+      <MyName />
+
+      </div>
+
+
+
+  );
+}
+
+export default App;
---
+++ src/MyName.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MyName.css';
+
+const MyName =() => {
+  return (
+    <div className="name">
+        Илья Карташов
+
+
+      </div>
+
+
+
+  );
+}
+
+export default MyName ;
